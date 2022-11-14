7a618694d3cc0c73f75b110095683af64bd7b72f
sergio estevao <up201905680@up.pt>
2022-11-14 16:30:03 +0000

    fix: xml scene

--- tp2/XMLscene.js
+++ tp2/XMLscene.js
@@ -162,7 +162,7 @@ export class XMLscene extends CGFscene {
         this.applyViewMatrix();
 
         this.pushMatrix();
-        //this.axis.display();
+        this.axis.display();
 
         for (var i = 0; i < this.lights.length; i++) {
             this.lights[i].setVisible(false);
