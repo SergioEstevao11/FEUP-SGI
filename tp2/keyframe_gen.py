375e3d91d3e50b60440f7b15e86f1ddff247f126
sergio estevao <up201905680@up.pt>
2022-11-21 23:54:16 +0000

    feat: tiefighter parabole animation

--- tp2/keyframe_gen.py
+++ tp2/keyframe_gen.py
@@ -21,7 +21,7 @@ def bullet_animation():
 
     f.close()
 
-def tiefighter_animation():
+def tiefighter_helix():
     x_distance = -20
     y_distance = 0
     z_distance = 0
@@ -42,8 +42,31 @@ def tiefighter_animation():
         current_x_distance += x_distance_per_frame
 
     f.close()
+
+def tiefighter_parabola_yz():
+    x_distance = -50
+    y_distance = 0
+    z_distance = 0
+
+    duration = 10
+    frames_per_second = 30
+    x_distance_per_frame = x_distance / (duration * frames_per_second)
+
+    current_x_distance = 0
+    instant = 8
+    f = open("./keyframe_gen_output.txt", "w")
+    while(current_x_distance > x_distance):
+        f.write('<keyframe instant="' + str(instant)  + '">\n')
+        f.write('     <translate x="' + str(current_x_distance) + '" y="' + str(4*math.sin(math.radians(180*(current_x_distance/x_distance)))) + '" z="' + str(1*math.sin(math.radians(180*(current_x_distance/x_distance)))) + '"/>\n')
+        f.write('     <rotate axis="z" angle="' + str(-15*math.cos(math.radians(180*(current_x_distance/x_distance)))) + '" />\n')
+        f.write('     <rotate axis="y" angle="' + str(30*math.cos(math.radians(180*(current_x_distance/x_distance)))) + '" />\n')
+        f.write("</keyframe>\n")
+        instant += 1/frames_per_second
+        current_x_distance += x_distance_per_frame
+
+    f.close()
 def main():
-        tiefighter_animation()
+        tiefighter_parabola_yz()
 
 if __name__ == "__main__":
     main()
