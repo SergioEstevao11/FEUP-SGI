83a08183d21015aee76cc6a4855bc3ffbf82c9f4
tomasnr13 <up201906743@fe.up.pt>
2022-12-24 13:21:45 +0000

    update

--- tp3/MyGameMove.js
+++ tp3/MyGameMove.js
@@ -4,12 +4,12 @@ import {CGFappearance, CGFtexture, CGFobject} from '../lib/CGF.js';
  * Data Class that holds information about the component
  */
 export class MyGameMove extends CGFobject{
-    constructor(scene, id) {
+    constructor(scene, piece, origin, deest, prevmove) {
         super(scene)
-        this.movedpiece = id;
-        this.origin = mat4.create();
-        this.dest = [];
-        this.statebef
+        this.movedpiece = piece;
+        this.origin = origin;
+        this.dest = dest;
+        this.statebef = prevmove
 	}
 
     animate(){
--- tp3/MyGameSequence.js
+++ tp3/MyGameSequence.js
@@ -4,17 +4,17 @@ import {CGFappearance, CGFtexture, CGFobject} from '../lib/CGF.js';
  * Data Class that holds information about the component
  */
 export class MyGameSequence extends CGFobject{
-    constructor(scene, id) {
+    constructor(scene) {
         super(scene)
         this.sequence = [];
 	}
 
     addMove(move){
-
+        this.sequence.push(move)
     }
 
     undo(){
-
+        this.sequence.pop()
     }
 
     moveReplay(coords){
